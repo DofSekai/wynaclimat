2aca9fcdcad3831b3ba751b70e98262654d3d30c
valentin.mantez <mantezvalentin@gmail.com>
2023-12-08 04:34:43 +0100

    feat(accueil.vue) : v2

--- resources/js/components/Accueil.vue
+++ resources/js/components/Accueil.vue
@@ -1,9 +1,17 @@
 <template>
     <div
-        class="bg"
+        class="bg d-flex align-items-center justify-content-center"
         :style="{ backgroundImage: 'url(' + backgroundImageUrl + ')' }"
     >
-        <div class="container"></div>
+        <div class="container text-center text-white">
+            <h1 class="title-container"><strong>Bienvenue sur WynaClimat !</strong></h1>
+            <h3 class="text-container">
+                Prêt à jouer un rôle crucial dans la création d'une fresque
+                climatique inspirante ? 🌍 Cliquez ci-dessous pour démarrer
+                l'aventure vers un avenir plus durable ! 🌱🎨
+            </h3>
+            <button class="btn btn-danger btn-lg">Cliquez ici</button>
+        </div>
     </div>
 </template>
 
@@ -19,9 +27,39 @@ export default {
 
 <style scoped>
 .bg {
+    position: relative;
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
     height: 100vh;
+}
+
+/* .bg::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(
+        0,
+        0,
+        0,
+        0.5
+    );
+} */
+.text-container {
+  max-width: 600px;
+  margin: 0 auto; 
+}
+
+.title-container {
+  font-size: 90px;
+  margin-bottom: 20px;
+}
+
+.btn-danger {
+    color: white;
+    margin-top: 50px;
 }
 </style>
--- resources/js/components/Contact.vue
+++ resources/js/components/Contact.vue
@@ -1,28 +1,67 @@
 <template>
-    <div
-        class="bg"
-        :style="{ backgroundImage: 'url(' + backgroundImageUrl + ')' }"
-    >
-        <div class="container"></div>
+    <div class="bg" :style="{ backgroundImage: 'url(' + backgroundImageUrl + ')' }">
+      <div class="container">
+        <div class="contact-container">
+          <div class="header">
+            <h1>CONTACTEZ-NOUS</h1>
+          </div>
+          <div class="form">
+            <div class="form-group">
+              <label for="email">Adresse Email:</label>
+              <input type="email" id="email" name="email" class="form-control" placeholder="Votre adresse email" />
+            </div>
+            <div class="form-group">
+              <label for="message">Contenu du Message:</label>
+              <textarea id="message" name="message" class="form-control" rows="5" placeholder="Écrivez votre message ici"></textarea>
+            </div>
+            <button class="btn btn-primary">Envoyer</button>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     data() {
-        return {
-            backgroundImageUrl: "./images/contact.png",
-        };
+      return {
+        backgroundImageUrl: "./images/contact.png",
+      };
     },
-};
-</script>
-
-<style scoped>
-.bg {
+  };
+  </script>
+  
+  <style scoped>
+  .bg {
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
     height: 100vh;
-}
-</style>
-
+  }
+  
+  .contact-container {
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px;
+    margin-top: 50px;
+  }
+  
+  .header {
+    text-align: center;
+  }
+  
+  .form {
+    margin-top: 20px;
+  }
+  
+  .form-group {
+    margin-bottom: 15px;
+  }
+  
+  .btn {
+    display: block;
+    margin-top: 15px;
+    width: 100%;
+  }
+  </style>
+  
