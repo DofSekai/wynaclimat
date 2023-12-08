93ab2f9850050d6a50a3a1dcbf41f21cbab91ca7
valentin.mantez <mantezvalentin@gmail.com>
2023-12-08 04:01:32 +0100

    feat(Accueil.vue)

--- resources/js/components/Accueil.vue
+++ resources/js/components/Accueil.vue
@@ -1,6 +1,26 @@
 <template>
-
-</template>
-
-<script>
-</script>
+    <div class="bg" :style="{ backgroundImage: 'url(' + backgroundImageUrl + ')' }">
+      <div class="container">
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        backgroundImageUrl: '../../../public/images/accueil.png',
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .bg {
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100vh;
+  }
+  </style>
+  
